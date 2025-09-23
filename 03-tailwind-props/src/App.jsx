fc5c75e7b0a87628652bacc3683d20a7946f627f
SAHIL GUPTA <sahilgupta3030@gmail.com>
2025-09-23 16:31:54 +0530

    Add theoretical comments

--- 03-tailwind-props/src/App.jsx
+++ 03-tailwind-props/src/App.jsx
@@ -1,10 +1,20 @@
 import Card from "./components/Card";
+// Importing Card component from folder.
+// Components let us reuse UI with different data.
 
 function App() {
   return (
+    // JSX looks like HTML but it calls a React function !
+    // Props are passed as parameters through this HTML-like syntax !
+
     <>
-      <Card username="sahilgupta" role="softwareengineer"/>
-      <Card username="abhishekthakre" role="dataprocessor"/>
+      {/* Using Card component with props (custom data) */}
+      <Card username="sahilgupta" role="softwareengineer" />
+
+      {/* Another Card with different props */}
+      <Card username="abhishekthakre" role="dataprocessor" />
+
+      {/* Card without props → default values will be used */}
       <Card />
     </>
   );
